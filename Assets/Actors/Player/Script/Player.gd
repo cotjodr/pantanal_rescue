58babd125000265713e3046cef08ea78086bb443
Rodrigo Sanches Devigo <cotjo.dr@ifms.edu.br>
2021-09-12 13:24:27 -0400

    Adicionando os movimentos do player.

--- Assets/Actors/Player/Script/Player.gd
+++ Assets/Actors/Player/Script/Player.gd
@@ -1,16 +1,47 @@
 extends Actor
 
+onready var Helicopter = $Helicopter
+onready var Gancho = $Gancho
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var motion = Vector2();
+
+func cartesian_to_isometric(cartesian):
+	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / 2)
+
+func _physics_process(delta):
+	
+	moveDir.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	moveDir.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	if Input.is_action_pressed("ui_select"): 
+		Gancho.animation = "Gancho_on"
+	else:
+		Gancho.animation = "Gancho_idle"
+	motion = moveDir.normalized() * speed
+	motion = cartesian_to_isometric(motion)
+	
+	move_and_slide(motion)
+
+func _process(delta):
+	animation_set(delta)
+	pass
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func animation_set(delta):
+	match moveDir:
+		Vector2(-1, 0):
+			Helicopter.animation = "Back_left"
+		Vector2(1,0):
+			Helicopter.animation = "Front_right"
+		Vector2(0, 1):
+			Helicopter.animation = "Front_left"
+		Vector2(0, -1):
+			Helicopter.animation = "Back_right"
+		Vector2(-1, -1):
+			Helicopter.animation = "Back"
+		Vector2(1, 1):
+			Helicopter.animation = "Front"
+		Vector2(1, -1):
+			Helicopter.animation = "Right"
+		Vector2(-1, 1):
+			Helicopter.animation = "Left"
